56b28724ce0f8db3a696f7b2be558c764dc76fef
Renato <renatorp2005@gmail.com>
2026-09-01 13:19:59 -0300

    Graphs generation

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -14,3 +14,22 @@ print(products_by_volume['Quantity'])
 products_by_revenue = df_grouped.sort_values(by='TotalSales', ascending=False)
 print("\n--- Ranking Revenue (R$) ---")
 print(products_by_revenue['TotalSales'])
+
+##generating graphs and sending to the 'graphs' folder
+import matplotlib.pyplot as plt
+plt.figure(figsize=(12, 8))
+plt.bar(products_by_volume.index, products_by_volume['Quantity'], color='#22153C')
+plt.title('Ranking by Volume of Sales')
+plt.xlabel('Product')
+plt.ylabel('Quantity Sold')
+plt.savefig('graphs/ranking_volume_sales.png')
+plt.show()
+
+##graph for revenue##
+plt.figure(figsize=(12, 8))
+plt.bar(products_by_revenue.index, products_by_revenue['TotalSales'], color='#11686A')
+plt.title('Ranking Revenue (R$)')
+plt.xlabel('Product')
+plt.ylabel('Total Revenue (R$)')
+plt.savefig('graphs/ranking_revenue.png')
+plt.show()
